28c7cd23ebfb8694d7a1ba6a82f4ba590d696435
Admen-UltraSoft <admenn94@gmail.com>
2023-01-20 21:40:04 +0100

    added fecth

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("Promena");
+fetch("https://jsonplaceolder.typicode.com/posts")
+  .then(function (call) {
+    return call.json();
+  })
+  .then(function (jsoncall) {
+    console.log(jsoncall);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
