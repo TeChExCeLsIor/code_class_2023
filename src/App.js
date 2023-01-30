0b79fb14b9b2b1a5e07fe6509092f91e5afc355f
Admen-UltraSoft <admenn94@gmail.com>
2023-01-30 19:02:23 +0100

    reset starter app

--- src/App.js
+++ src/App.js
@@ -8,22 +8,9 @@ import React from "react";
 // uslov ? (druguslov ? ako tocno: ako netocno) : else tuka
 
 function App() {
-  const broj = 4;
-  const isBlack = true;
-  const nekojSiBroj = broj >= 5 ? 55 : 155;
   return (
     <div>
-      <h1>
-        {broj > 5 ? "Brojot e pogolem od 5 ||" : "Brojot e pomal od 5 ||"}
-        {nekojSiBroj}
-      </h1>
-      <h1>
-        {isBlack === true && "Bojata e crna"}
-        {!isBlack && "Bojata ne e crna"}
-      </h1>
-      {isBlack === true && (
-        <div style={{ width: 150, height: 150, backgroundColor: "black" }} />
-      )}
+      <h1>Hello World!</h1>
     </div>
   );
 }
