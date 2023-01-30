5aaef9e401b01849bac3c14b1fd5bdbb7a4ced91
Admen-UltraSoft <admenn94@gmail.com>
2023-01-30 19:00:51 +0100

    Ternary expresion

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from "react";
 
+// ternary expression
+// uslov ? ako uslovot e tocen : ako uslovot e netocen
+
+// uslov && shto sakame da se renderira ako uslovot e tocen
+
+// uslov ? (druguslov ? ako tocno: ako netocno) : else tuka
+
 function App() {
+  const broj = 4;
+  const isBlack = true;
+  const nekojSiBroj = broj >= 5 ? 55 : 155;
   return (
     <div>
-      <h1>Hello world</h1>
+      <h1>
+        {broj > 5 ? "Brojot e pogolem od 5 ||" : "Brojot e pomal od 5 ||"}
+        {nekojSiBroj}
+      </h1>
+      <h1>
+        {isBlack === true && "Bojata e crna"}
+        {!isBlack && "Bojata ne e crna"}
+      </h1>
+      {isBlack === true && (
+        <div style={{ width: 150, height: 150, backgroundColor: "black" }} />
+      )}
     </div>
   );
 }
