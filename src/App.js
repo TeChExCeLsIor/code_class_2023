4439653657bfdb6231c3e01ab4eeb940e6672215
Admen-UltraSoft <admenn94@gmail.com>
2023-02-08 18:53:54 +0100

    fala Samil

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import React from "react";
 
-// ternary expression
-// uslov ? ako uslovot e tocen : ako uslovot e netocen
-
-// uslov && shto sakame da se renderira ako uslovot e tocen
-
-// uslov ? (druguslov ? ako tocno: ako netocno) : else tuka
-
 function App() {
   return (
     <div>
