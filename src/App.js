798ac595e75d8cb8ac1e985dcc586b99be83eb3a
Admen-UltraSoft <admenn94@gmail.com>
2023-02-08 20:29:36 +0100

    Create typing game in class

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
-import React from "react";
-
-// ternary expression
-// uslov ? ako uslovot e tocen : ako uslovot e netocen
-
-// uslov && shto sakame da se renderira ako uslovot e tocen
-
-// uslov ? (druguslov ? ako tocno: ako netocno) : else tuka
+import React, { useState, useEffect, useRef } from "react";
 
 function App() {
+  const [isGameStarted, setIsGameStarted] = useState(false);
+  const [timer, setTimer] = useState(5);
+  const [userInput, setUserInput] = useState("");
+  const [score, setScore] = useState(0);
+  // const [disableTextArea, setDisableTextArea] = useState(true);
+
+  const textareaRef = useRef();
+
+  useEffect(() => {
+    if (timer > 0 && isGameStarted) {
+      setTimeout(() => setTimer((prev) => prev - 1), 1000);
+    } else {
+      endGame();
+    }
+  }, [timer, isGameStarted]);
+
+  const startGame = () => {
+    // setDisableTextArea(false);
+    textareaRef.current.disabled = false;
+    textareaRef.current.focus();
+    setTimer(5);
+    setIsGameStarted(true);
+    setUserInput("");
+  };
+
+  const endGame = () => {
+    // setDisableTextArea(true);
+    textareaRef.current.disabled = true;
+    setIsGameStarted(false);
+    const arrayOfWords = userInput.split(" ");
+    const filteredArrayOfWords = arrayOfWords.filter((el) => el !== "");
+    setScore(filteredArrayOfWords.length);
+  };
+
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className="gameContainer">
+      <h1>Typing Game</h1>
+      <textarea
+        ref={textareaRef}
+        disabled={true}
+        value={userInput}
+        onChange={(e) => setUserInput(e.target.value)}
+      />
+      <div>
+        <button onClick={startGame}>Start Game</button>
+      </div>
+      <h4>Time left: {timer}</h4>
+      <h4>Score: {score}</h4>
     </div>
   );
 }
